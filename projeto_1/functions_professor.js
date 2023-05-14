5193c86499d74f0b4b9ed3908df81946283316fb
thiago-anjos <your_email@example.com>
2023-05-13 22:38:23 -0300

    wip - solucao do professor para ler legenda

---
+++ projeto_1/functions_professor.js
@@ -0,0 +1,55 @@
+const fs = require("fs");
+
+function readDirectory(path, customPath) {
+    return new Promise((resolve, reject) => {
+        try {
+            let files = fs.readdirSync(customPath);
+            files = files.map((file) => path.join(customPath, file));
+            resolve(files);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+function readFile(customPath) {
+    return new Promise((resolve, reject) => {
+        try {
+            const content = fs.readFileSync(customPath, { encoding: "utf8" });
+            resolve(content);
+        } catch (error) {
+            reject(error);
+        }
+    });
+}
+
+function readFiles(paths) {
+    return Promise.all(paths.map((path) => readFile(path)));
+}
+
+function filesExtension(array, extension) {
+    return array.filter((file) => file.endsWith(extension));
+}
+
+function removeEmptyString(array) {
+    return array.filter((el) => el.trim());
+}
+
+function removeTimeOnLines(array, defaultText) {
+    return array.filter((el) => !el.includes(defaultText));
+}
+
+function removeLinesNumbers(array) {
+    const patternOnlyNumbers = /[0-9]/g;
+    return array.map((el) => el.replace(patternOnlyNumbers, ""));
+}
+
+module.exports = {
+    readDirectory,
+    filesExtension,
+    readFile,
+    readFiles,
+    removeEmptyString,
+    removeTimeOnLines,
+    removeLinesNumbers,
+};
---
+++ projeto_1/index_professor.js
@@ -0,0 +1,15 @@
+const path = require("path");
+const fn = require("./functions_professor");
+
+const customPath = path.join(__dirname, "legendas");
+
+const files = fn
+    .readDirectory(path, customPath)
+    .then((files) => fn.filesExtension(files, "srt"))
+    .then((files) => fn.readFiles(files))
+    .then((contents) => contents.join(""))
+    .then((allContent) => allContent.split("\n"))
+    .then(fn.removeEmptyString)
+    .then((lines) => fn.removeTimeOnLines(lines, "-->"))
+    .then(fn.removeLinesNumbers)
+    .then((value) => console.log(value));
