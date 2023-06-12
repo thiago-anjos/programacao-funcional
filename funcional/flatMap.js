664132d5a9f246962fe666a6f85beab878ee9cf7
thiago-anjos <your_email@example.com>
2023-06-11 21:54:05 -0300

    flatmap

---
+++ funcional/flatMap.js
@@ -0,0 +1,7 @@
+// const letters = ['h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd'];
+// const result = letters.map((letter) => letter.toUpperCase()).join('');
+// console.log(result);
+
+const letters = [['h', 'e', 'l', 'l', 'o'], [' '], ['w', 'o', 'r', 'l', 'd']];
+console.log(letters);
+console.log(letters.flat());
