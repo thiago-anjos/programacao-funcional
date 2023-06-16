8ce0ce29706ba44981b963b52a869cfe71172448
thiago-anjos <your_email@example.com>
2023-06-15 23:21:56 -0300

    rxjs - stream de dados

---
+++ rxjs/stream_dados.js
@@ -0,0 +1,19 @@
+const { interval } = require('rxjs');
+
+const gerarNumeros = interval(500);
+
+const sub1 = gerarNumeros.subscribe((num) => {
+    console.log(Math.pow(2, num));
+});
+
+setTimeout(() => {
+    sub1.unsubscribe();
+}, 5000);
+
+const sub2 = gerarNumeros.subscribe((letter) => {
+    console.log(letter);
+});
+
+setTimeout(() => {
+    sub2.unsubscribe();
+}, 5000);
